23206697b57a104d55b0eb445c61e7f69814d6f9
sansk <iamsangyk@gmail.com>
2023-06-05 11:35:50 +0530

    sidebar update

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -13,6 +13,28 @@ export const Sidebar = () => {
           <FaTimes />
         </button>
       </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
     </aside>
   );
 };
